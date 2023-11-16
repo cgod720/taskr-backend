9b535bad2e0181c53b4886a2209847d7a4fd5a61
carlos-godoy720 <bit.krypton@protonmail.com>
2023-11-16 15:32:39 -0500

    added tokens to createUser function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,8 +2,10 @@ const express = require('express')
 const users = express.Router()
 require('dotenv').config()
 const secretKey = process.env.SECRET
+const jwt = require('jsonwebtoken')
 
 const { createUser, getUsers } = require('../queries/users')
+const { authenticateToken } = require('../auth/auth')
 
 
 
@@ -19,12 +21,12 @@ users.get('/', async (req, res) => {
 users.post('/', async (req, res) => {
     try {
         const newUser = await createUser(req.body)
-        const token = jwt.sign({ userId: createdUser.user_id, username: createdUser.username }, secretKey);
+        const token = jwt.sign({ userId: newUser.user_id, username: newUser.username }, secretKey);
 
         res.status(201).json({ user: newUser, token });
-        // res.status(201).json({ user: newUser })
     } catch (error) {
-        res.status(500).json({ error: "Invalid information"})
+        // console.log(error)
+        res.status(500).json({ error: "Invalid information", info: error })
     }
 })
 
